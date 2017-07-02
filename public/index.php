63c34aa32070322b02afacde95f68e15bd412241
Diego Moreno <diegomoreno@cyandark.com>
2017-07-02 00:06:18 -0500

    Scripts in Applications.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,8 @@
 <?php
-use Illuminate\Http\Request;
-use PlatziPHP\Http\Controllers\HomeController;
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-$container = new \Illuminate\Container\Container();
-
-$router = new \Illuminate\Routing\Router(
-    new \Illuminate\Events\Dispatcher($container),
-    $container
+$app = new \PlatziPHP\Application(
+    new \Illuminate\Container\Container()
 );
 
-$router->get('/', HomeController::class . '@index');
-$router->get('/post/{id}', HomeController::class . '@show');
-
-$response = $router->dispatch(Request::capture());
-
-$response->send();
+$app->run();
